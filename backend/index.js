c24de2fbf116c00d1984081cacb90f590a2a94eb
Ananya Melody <ananyamelody29@gmail.com>
2026-09-22 10:47:13 +0530

    2nd commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
-let express=require('express');
-let app=express();
-let hrroutes=require('./routes/hr_routes')
+let express = require('express');
+let mongoose = require('mongoose');
 
-app.use("/api/hr",hrroutes);
-//localhost:2829/api/hr/viewemployees
+let app = express();
 
+let hrroutes = require('./routes/hr_routes');
+let emproutes =require('./routes/emp_routes');
+app.use(express.json());
 
+mongoose.connect("mongodb://localhost:27017/hr_management")
+    .then(() => {
+        console.log("DB connection success");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-//run server
-app.listen(2829,()=>{
+app.use("/api/hr", hrroutes);
+app.use("/api/emp",emproutes);
+
+// localhost:2829/api/hr/viewemployees
+
+// run server
+app.listen(2829, () => {
     console.log("server listening on port 2829");
-})
+});
---
+++ backend/routes/emp_routes.js
@@ -0,0 +1,21 @@
+let express=require('express');
+let router=express.Router();
+let {users}=require('../models/users');
+let bcrypt=require('bcrypt');
+router.post("/register",async (req,res)=>{
+    let data = req.body;
+    data.password=await bcrypt.hash(data.password,10);
+    let newuser=new users(data);
+    let result=await newuser.save();
+    res.send(result);
+})
+
+router.post("/login",(req,res)=>{
+    res.send("login page called");
+})
+
+router.get("/viewtask",(req,res)=>{
+    res.send("view task page called")
+})
+
+module.exports=router;
